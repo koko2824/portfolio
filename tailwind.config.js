96d40002c8658f15f83577a436ca75e4e632c000
koko2824 <prg.koko2824@gmail.com>
2021-02-25 06:52:09 +0900

    fix: setting tailwndcss

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,17 @@ module.exports = {
   purge: ['./pages/**/*.tsx', './components/**/*.tsx'],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        blue: '#00E6EF',
+        green: '#00EFC1',
+        white: '#D6E6F1',
+        bg: '#094152',
+      },
+      fontFamily: {
+        sans: ['Arial']
+      }
+    },
   },
   variants: {
     extend: {},
