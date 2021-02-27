bd620cbe1aac46070973d1bed638b2518b03a269
koko2824 <prg.koko2824@gmail.com>
2021-02-27 15:22:10 +0900

    feat: skillBar Component

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,23 @@
 import Header from './components/Header'
+import SkillBar from './components/SkillBar'
+
 export default function Home() {
   return (
-    <div className="">
+    <>
       <Header />
-      <p className=" text-3xl text-white font-light">hello world</p>
-    </div>
+      <div className="container mx-auto">
+        <SkillBar
+          rows={[
+            { title: 'HTML', percent: 90 },
+            { title: 'TypeScript', percent: 50 },
+            { title: 'Node.js', percent: 70 },
+            { title: 'React.js', percent: 60 },
+            { title: 'tailwindcss', percent: 70 },
+            { title: 'Golang', percent: 70 },
+            { title: 'Firebase', percent: 60 },
+          ]}
+        />
+      </div>
+    </>
   )
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,8 +3,24 @@ module.exports = {
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {
+      height: {
+        'px': '2px'
+      },
+      width: {
+        '1/10': '10%',
+        '2/10': '20%',
+        '3/10': '30%',
+        '4/10': '40%',
+        '5/10': '50%',
+        '6/10': '60%',
+        '7/10': '70%',
+        '8/10': '80%',
+        '9/10': '90%',
+        '10/10': '100%',
+      },
       colors: {
         white: '#d6e6f1',
+        'dark-white': '#E0DCDC',
         'light-blue': '#04c2c9',
         'dark-blue': '#04c2c9BF',
         'light-black': '#6B7280BF',
