76f916639d34a36dc74e3dc6e9f18e5b32d084ff
koko2824 <prg.koko2824@gmail.com>
2021-03-08 10:35:34 +0900

    feat: Sidebar Component

--- tailwind.config.js
+++ tailwind.config.js
@@ -29,6 +29,10 @@ module.exports = {
       fontFamily: {
         sans: ['Arial'],
       },
+      boxShadow: {
+        '3xl': '0 35px 60px -15px rgba(0, 0, 0, 0.3)',
+        '4xl': '0 45px 70px -18px rgba(0, 0, 0, 0.3)',
+      },
     },
   },
   variants: {
