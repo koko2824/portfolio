db68e0cfc3040f6da950753a63dd8a6da2c6e6a1
koko2824 <prg.koko2824@gmail.com>
2021-03-10 12:17:24 +0900

    feat: animation

--- components/sidebar.tsx
+++ components/sidebar.tsx
@@ -1,6 +1,6 @@
 import React, { ReactElement, useState } from 'react'
 import Link from 'next/link'
-import { css, cx } from '@emotion/css'
+import { css, cx, keyframes } from '@emotion/css'
 import { ArrowLeft, ArrowRight, Home, User, Code, ViewGrid, MailOutline } from 'heroicons-react'
 
 interface sidebarProps {
@@ -9,40 +9,54 @@ interface sidebarProps {
 
 export default function Sidebar(props: sidebarProps) {
   const [show, setShow] = useState(true)
+  const links = [
+    { icon: <Home className="mt-16" size={32} />, href: '/' },
+    { icon: <User className="mt-8" size={32} />, href: '#' },
+    { icon: <Code className="mt-8" size={32} />, href: '#' },
+    { icon: <ViewGrid className="mt-8" size={32} />, href: '#' },
+    { icon: <MailOutline className="mt-8" size={32} />, href: '#' },
+  ]
+  const slideIn = keyframes`
+  from { transform: translateX(-3rem) }
+  to { transform: translateX(0)}
+  `
+
   if (show) {
     return (
-      <div className="flex text-white">
+      <div className="flex text-white w-screen">
         <div
           className={cx(
+            ' absolute top-0 left-0 px-5',
             css`
-              width: 70px;
-            `,
-            'h-screen sticky top-0 flex justify-center shadow-4xl'
+              animation: ${slideIn} .3s;
+            `
           )}
         >
-          <div>
-            <a>
-              <ArrowLeft className="my-8" onClick={() => setShow(false)} size={32} />
-            </a>
-            <Link href="/">
-              <a>
-                <Home className="mt-16" size={32} />
-              </a>
+          <a>
+            <ArrowLeft className="my-8" onClick={() => setShow(false)} size={32} />
+          </a>
+          {links.map((link, i) => (
+            <Link href={link.href} key={i}>
+              <a> {link.icon} </a>
             </Link>
-            <User className="mt-8" size={32} />
-            <Code className="mt-8" size={32} />
-            <ViewGrid className="mt-8" size={32} />
-            <MailOutline className="mt-8" size={32} />
-          </div>
+          ))}
         </div>
-        <div>{props.children}</div>
+        <div className="">{props.children}</div>
       </div>
     )
   } else {
     return (
-      <div className={cx(css``, 'bg-normal-black text-white')} onClick={() => setShow(true)}>
+      <div className="bg-normal-black text-white" onClick={() => setShow(true)}>
         <a>
-          <ArrowRight className="absolute top-8 left-8" size={32} />
+          <ArrowRight
+            className={cx(
+              'absolute top-8 left-8',
+              css`
+                animation: ${slideIn} .3s ease-out;
+              `
+            )}
+            size={32}
+          />
         </a>
         {props.children}
       </div>
